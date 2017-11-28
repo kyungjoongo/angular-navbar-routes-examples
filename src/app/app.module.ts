9fe001928e15f49f6792d7cb2650e2435e5df7d1
kyungjoongo <kyungjoon.go@gmail.com>
2017-11-28 17:03:56 +0900

    observable example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,41 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
-
 import {AppComponent} from './app.component';
 import {Test001Component} from './test001/test001.component';
 import {HomeComponent} from './home/home.component';
 import {AboutComponent} from './about/about.component';
 import {RouterModule, Routes} from "@angular/router";
-import { NavbarComponent } from './navbar/navbar.component';
-import { DietComponent } from './diet/diet.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {DietComponent} from './diet/diet.component';
+import {DataService} from "./services/data.service";
+import {HttpModule} from "@angular/http";
 
-const appRoutes : Routes =[
-  {path : '' , component : HomeComponent},
-  {path : 'about' , component: AboutComponent},
-  {path : 'diet' , component: DietComponent},
+const appRoutes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'diet', component: DietComponent},
 ];
 
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    Test001Component,
-    HomeComponent,
-    AboutComponent,
-    NavbarComponent,
-    DietComponent
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(appRoutes),
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        Test001Component,
+        HomeComponent,
+        AboutComponent,
+        NavbarComponent,
+        DietComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(appRoutes),
+        HttpModule,
+    ],
+    providers: [
+        DataService,
+
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
